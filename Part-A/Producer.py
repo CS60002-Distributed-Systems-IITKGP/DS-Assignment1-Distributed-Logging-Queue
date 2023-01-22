23c8452696f39816aa9f9c69caecb5f773e6c9d2
Dmitra1993 <mitradevd@gmail.com>
2023-01-22 20:31:42 +0530

    adding comment

--- Part-A/Producer.py
+++ Part-A/Producer.py
@@ -15,7 +15,7 @@ class Producer(BaseModel):
     message: str
    
     
-
+# POST for producer registration
 @app.post("/{producer_id}/{topic}/register")
 def producer_registration(producer_id: int, topic):
     return {'Data':{'producer_id':producer_id,
