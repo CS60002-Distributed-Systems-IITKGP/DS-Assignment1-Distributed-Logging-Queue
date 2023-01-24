aca36a6c8c7fad48e28313abb6ef65d2f4b89211
soura1819 <sourabhatta1819@gmail.com>
2023-01-24 22:35:06 +0530

    error handled

--- Part-A/consumer.py
+++ Part-A/consumer.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi import HTTPException
 # from fastapi import APIRouter
 
 # router = APIRouter()
@@ -10,17 +11,22 @@ class Consumer(BaseModel):
     id: int
     topic: str
 
+topics = []
 
 # POST for registering user
-@app.post('/{consumer_id}/{topic}/register')
-def register_consumer(consumer_id: int, topic):
+@app.post('/{consumer_id}/{topic_id}/register')
+def register_consumer(consumer_id: int, topic_id: int):
+    if topic_id not in topics:
+        raise HTTPException(status_code=404, detail="Topic not found")
     return {'Data':{'ConsummerId':consumer_id,
-                    'Topic':topic}}
+                    'Topic':topic_id}}
 
 
 # GET for dequeue
-@app.get('/{consumer_id}/{topic}')
-def dequeue_topic(consumer_id: int, topic):
+@app.get('/{consumer_id}/{topic_id}')
+def dequeue_topic(consumer_id: int, topic_id: int):
+    if topic_id not in topics:
+        raise HTTPException(status_code=404, detail="Topic not found")
     return {'Data':{'ConsummerId':consumer_id,
-                    'Topic':topic}}
+                    'Topic':topic_id}}
 
