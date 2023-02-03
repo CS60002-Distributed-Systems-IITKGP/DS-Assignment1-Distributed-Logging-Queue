0f174e231ba051b6d054cd787749442f95a01cb2
Sairaj Das <sairajd044@gmail.com>
2023-02-04 00:02:12 +0530

    Modified get_next in Consumer

--- Part-C/Consumer.py
+++ Part-C/Consumer.py
@@ -1,4 +1,7 @@
 import requests
+import threading
+import time
+import asyncio
 
 class MyConsumer():
     def __init__(self, broker, topics=[]):
@@ -7,38 +10,49 @@ class MyConsumer():
         for topic in topics:
             self.__subscribe(topic)
 
-    def get_next(topic_name):
+    async def get_next(self, topic_name):
         if topic_name not in self.subbedTopics:
             print(f'Not subscribed to the topic : {topic_name}')
         else:
-            consumer_id = self.subbedTopics[topic_name]
-            msg = self.__recvMsg(topic_name, consumer_id)
-            # if msg:
-            #     print(f'New message in {topic_name} : {msg}')
-            #     return msg
-            # else:
-            #     print(f'No new message in this topic!')
-            #     return None
-            return msg
+            while True:
+                consumer_id = self.subbedTopics[topic_name]
+                msg = self.__recvMsg(topic_name, consumer_id)
+                if msg:
+                    print(f'New message in {topic_name} : {msg}')
+                await asyncio.sleep(5)
+                    # return msg
+                # else:
+                #     print(f'No new message in this topic!')
+                    # return None
+                
+            # return msg
 
-    def __subscribe(topic_name):
-        url = self.broker + f'/consumer/register/{topic_name}'
+    def __subscribe(self, topic_name):
+        url = f'http://{self.broker}/consumer/register/{topic_name}'
         response = requests.post(url)
         if response.status_code == 404:
-            print(f'Failed to subscibbed to {topic_name} : Topic not found')
+            print(f'Failed to subscibe to {topic_name} : Topic not found')
         elif response.status_code == 201:
-            response_body = r.json()
+            response_body = response.json()
             consumer_id = response_body["consumer_id"]
             self.subbedTopics[topic_name] = consumer_id
             print(f'Consumer {consumer_id} subscribbed to {topic_name}')
 
-    def __recvMsg(topic_name, consumer_id):
-        url = self.broker + f'/consumer/consume/{topic_name}/{consumer_id}'
+    def __recvMsg(self, topic_name, consumer_id):
+        url = f'http://{self.broker}/consumer/consume/{topic_name}/{consumer_id}'
         response = requests.get(url)
         if response.status_code == 404:
             return None
         if response.status_code == 200:
-            return r.json()
+            return response.json()
+
+    def size(self):
+        url = f'http://{self.broker}/size/{topic_name}/{consumer_id}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            response_body = response.json()
+            return int(response_body)
+        
             
     
     
--- Part-C/consumer_example.py
+++ Part-C/consumer_example.py
@@ -1,5 +1,12 @@
+import asyncio
 from Consumer import MyConsumer
 
-consumer = MyConsumer(broker = "localhost:8000", topics = ["abc", "xyz"])
-msg = consumer.get_next("abc")
-print(msg)
+async def main():
+    consumer = MyConsumer(broker = "localhost:8000", topics = ["abc", "xyz"])
+    task1 = asyncio.create_task(consumer.get_next("abc"))
+    task2 = asyncio.create_task(consumer.get_next("xyz"))
+    await task1
+    await task2
+
+asyncio.run(main())
+# consumer.get_next("xyz")
